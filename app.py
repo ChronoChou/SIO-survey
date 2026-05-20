c3d7f07936a67c7fd7d23c703b5f08da315e62cf
ChronoChou <undertaker3787@gmail.com>
2026-05-20 11:30:47 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -13,31 +13,36 @@ st.markdown("---")
 
 # 3. 問卷表單設計
 with st.form(key="sio_survey_form", clear_on_submit=True):
-    st.write("### 📥 第一階段：特務基本資料")
-    
-    # 填寫人姓名 (下拉選單，防止打錯字方便後續分析)
-    agent_name = st.selectbox(
-        "請選擇您的特務代號/稱呼：",
-        ["Ann Sui Kuo", "Liu Lian Chou", "Lao Oua Kuo", "Hang Hea Hu", "姐呼", "其他家族成員"]
-    )
-    
-    st.write("---")
-    st.write("### 🏔️ 第二階段：戰略營地部署投票")
-    
+   
     # 營地選擇 (單選題)
     camp_choice = st.radio(
-        "下次高海拔避暑行動，您傾向哪一個營地？(海拔 1000m+)",
-        ["春露茶園 (1100m) - 觀星與無敵雲海", "密靜莊園 (1200m) - 森林系獨立空間", "非比尋常 (1100m) - 厚實草皮包區"]
+        "1. 預計冬天再舉辦一次露營，地點同樣是小路露營區",
+        ["贊成", "有困難"]
     )
     
     # 飲食偏好 (複選題)
-    food_preference = st.multiselect(
-        "針對新成員 Hang Hea Hu 的熱能海鮮補給，您最期待吃到什麼？(可多選)",
-        ["招牌泰式烤蝦 🦐", "鹽烤大草蝦 🦐", "蒜蓉啤酒蝦 🍺", "不挑食，執行官烤的都吃！"]
+    Days_preference = st.radio(
+        "2. 希望安排幾天幾夜",
+        ["三天兩夜", "兩天一夜"]
+    )
+
+    Summer_preference = st.radio(
+        "3. 是否希望在暑假期間再安排一次家族旅遊，願意的話希望包棟還是露營?",
+        ["包棟", "高海拔露營(氣溫涼爽)", "沒意願"]
+    )
+
+    Summerday_preference = st.radio(
+        "4. 承上，如有意願希望安排天數?",
+        ["三天兩夜", "兩天一夜"]
+    )
+
+    Price_preference = st.radio(
+        "5. 因包棟價位較高，約露營的1.5-2.5倍，故可接受家族旅遊住宿(一晚)價位為?",
+        ["比小路再便宜", "小路 * 1~1.5" ,  "小路 * 1.5~2" ,"小路 * 2~2.5" ,"小路 * 2.5~3" , "無所謂"]
     )
     
     # 自由意見回饋 (簡答題)
-    suggestions = st.text_area("對於這次『群牛亂舞』行動的辛酸或未來建議，請在此留下您的機密留言：")
+    suggestions = st.text_area("對於家族旅遊有什麼建議，請在此留言：")
     
     # 4. 送出按鈕
     submit_button = st.form_submit_button(label="🚀 發送情資 (Submit)")
@@ -47,10 +52,11 @@ if submit_button:
     # 建立單筆紀錄資料夾
     survey_data = {
         "填寫時間": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        "特務代號": agent_name,
-        "偏好營地": camp_choice,
-        "期待蝦料理": ", ".join(food_preference),
-        "特務意見": suggestions
+        "冬天是否再小路舉辦露營": camp_choice,
+        "冬天舉辦天數": Days_preference,
+        "暑假舉辦型式": Summer_preference,
+        "暑假家旅天數": Summerday_preference,
+        "可接受價位": Price_preference,
     }
     
     # 資料存檔路徑 (本機測試時會存成 Excel)
